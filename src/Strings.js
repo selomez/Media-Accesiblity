f2f0802dd0792df132ffaca44dc949d8e512ee4e
selomez <selzerai01@gmail.com>
2021-10-15 09:55:01 -0700

    empty app + strings.js

---
+++ src/Strings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    LATER : 'Maybe Later',
+    DICTATE : 'Dictate',
+    TYPE: 'Type',
+    LATERFLOW1: 'Are you sure? Captioning your photo creates a more accessible environment' , 
+    CAPTIONCOMPLETE : 'Caption saved! Thank you for creating an accessible environment.',
+    PICNOCAPTION: 'You have chosen a photo with no caption, would you like to add a caption?',
+    UNCAPTIONEDPHOTO: 'You have uncaptioned photos in your gallery. Would you like to caption them now?',
+    NEWPHOTOSADDED: 'You have new photos added, would you like to add a caption? ', 
+}
