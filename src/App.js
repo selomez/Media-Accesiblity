60ddf5fa20dd4eca08f7eb0f63c21f0e4c362228
adr-23 <adriangabales1@gmail.com>
2021-10-31 19:04:49 -0700

    added react-bootstrap for buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import "./App.css";
-import  AppView  from "./newPhotosPopup.js";
+import  {AppView}  from "./newPhotosPopup.js";
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import {ImageTextInput,ImageTextInputMultiline} from "./textInput";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   
   return (
+    
     <div className="App">
       
+
       <header className="App-header">
-        <div className = "css-view-1dbjc4n">
-          <AppView />
-          <ImageTextInputMultiline />
-        </div>
+        
+        <ImageTextInputMultiline />
+        <Container>
+          <Row>
+            <Col>
+              <Button variant='danger'> Cancel </Button>
+            </Col>
+            <Col>
+              <Button variant='primary'> Submit </Button>
+            </Col>
+          </Row>
+          
+        </Container>
+        
+        
       </header>
     </div>
   );
