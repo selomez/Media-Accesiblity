ca539b442492e304a850cd88c5ff9145e6b25e93
zerais <zerais@wwu.edu>
2021-11-01 00:30:13 -0700

    adding image for test

--- src/App.js
+++ src/App.js
@@ -4,19 +4,30 @@ import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import {Image} from 'react-native'
 import {ImageTextInput,ImageTextInputMultiline} from "./textInput";
 import 'bootstrap/dist/css/bootstrap.min.css';
+//import {noCaptionPopUp} from 'photoNoCaption.js';
 
 
 function App() {
+
+  const message = () => {
+    console.log("clicked!") 
+   }
   
   return (
     
     <div className="App">
-      
+     
 
       <header className="App-header">
-        
+      <Image 
+        source = {require("mountain.jpeg")}
+        alt = "Hello World"
+        onPress = {message} 
+      />
+      
         <ImageTextInputMultiline />
         <Container className="mt-3">
           <Row>
