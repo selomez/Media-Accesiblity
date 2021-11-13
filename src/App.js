b57b819a1fc5a3c6acd1acc87a58976c3fd32b68
Ella Neumann <ellaneumann@Ellas-MacBook-Pro-2.local>
2021-11-13 12:02:47 -0800

    deleted test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <ImageTextInputMultiline/>
-        {<DisplayAnImage src="./mountain.jpeg" />}
       </header>
     </div>
   );
